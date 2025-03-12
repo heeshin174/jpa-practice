a82e2b749bb42387d819b621da539f655a3b8843
Heechul Shin <heeshin174@gmail.com>
2025-03-12 14:01:02 +0900

    Add item entity classes

---
+++ demo/src/main/java/com/example/demo/domain/item/Book.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter @Setter
+public class Book {
+    private String author;
+    private String isbn;
+}
