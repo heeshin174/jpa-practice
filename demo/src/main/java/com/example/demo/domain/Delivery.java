1ea4f71cd239fcc8b89a4dd47ea93fc2829d7636
Heechul Shin <heeshin174@gmail.com>
2025-03-12 13:57:19 +0900

    Add Entity class

--- demo/src/main/java/com/example/demo/domain/Delivery.java
+++ demo/src/main/java/com/example/demo/domain/Delivery.java
@@ -4,21 +4,22 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter @Setter
-public class Member {
+public class Delivery {
 
     @Id
     @GeneratedValue
+    @Column(name = "delivery_id")
     private Long id;
-    private String username;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
 
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
+
 }
